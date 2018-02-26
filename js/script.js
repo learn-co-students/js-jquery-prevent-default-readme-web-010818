4e9b929799a5abb572aa924f2713ecc077a6a0d8
GuttermanA <alexanderf.gutterman@gmail.com>
2018-02-26 17:01:37 -0500

    Done.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
-  
+
   // browser refreshes on submit
-  $('form').on('submit', function(){
+  $('form').on('submit', function(event){
     var name = $('#name').val();
     $("#hello").text("Hello, " + name);
+    event.preventDefault();
   });
 
   //examine event object
